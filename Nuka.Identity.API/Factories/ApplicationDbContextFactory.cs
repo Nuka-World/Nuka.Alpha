b7bcf9dc2ff45a54ba216c6fbfb99c11430a535b
Xiang Zhu <zhuxiang1212@hotmail.com>
2021-01-03 12:51:54 +0800

    IDesignTimeDbContextFactory for DbContexts

---
+++ Nuka.Identity.API/Factories/ApplicationDbContextFactory.cs
@@ -0,0 +1,30 @@
+﻿using System.IO;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+using Nuka.Identity.API.Data;
+
+namespace Nuka.Identity.API.Factories
+{
+    public class ApplicationDbContextFactory : IDesignTimeDbContextFactory<ApplicationDbContext>
+    {
+        public ApplicationDbContext CreateDbContext(string[] args)
+        {
+            var migrationAssembly = this.GetType().Assembly.GetName().Name;
+
+            var config = new ConfigurationBuilder()
+                .SetBasePath(Path.Combine(Directory.GetCurrentDirectory()))
+                .AddJsonFile("appsettings.json")
+                .AddEnvironmentVariables()
+                .Build();
+
+            var optionsBuilder = new DbContextOptionsBuilder<ApplicationDbContext>();
+
+            optionsBuilder.UseSqlServer(
+                config.GetConnectionString("DefaultConnection"),
+                sqlServerOptionsAction: o => o.MigrationsAssembly(migrationAssembly));
+
+            return new ApplicationDbContext(optionsBuilder.Options);
+        }
+    }
+}
